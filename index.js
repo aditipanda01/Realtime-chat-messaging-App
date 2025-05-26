1c76bfd582ccb91e661424e6c84bcb7654415806
Aditi Panda <pandaaditi37@gmsil.com>
2025-05-26 06:22:27 +0530

    backend commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+
